b9f62e269494ad1b907a134750ecebe25932addc
Josh Eads <josh.eads@sony.com>
2018-11-01 10:26:21 -0700

    new test

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdio.h>
 
 
-int
-main(int argc, char **argv)
+void
+test1(void)
 {
     char local_buffer[100];
     void *buffer = malloc(100);
 
     memcpy(buffer, local_buffer, 50);
+}
+
+void
+test2(void)
+{
+    uint16_t x = 0xfff0, y = 0x20;
+
+    if (x + y < x) {
+        // handle integer overflow
+        printf("overflow\n");
+    }
+}
+
+
+int
+main(int argc, char **argv)
+{
+    test1();
+    test2();
 }
